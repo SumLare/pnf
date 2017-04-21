dcada51b85a4a3bef019e7655233386774ae4337
SumLare <mrhard95@gmail.com>
2017-04-21 23:07:49 +0300

    Initial commit

---
+++ pnf/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+def main():
+    formula = input("Transform your formula to PNF: ")
+    while formula:
+        print(formula)
+        formula = input("Transform your formula to PNF: ")
+
+if __name__ == '__main__':
+    main()
