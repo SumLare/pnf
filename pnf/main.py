4ad68025360434eb5956d05542a9df7d0bf657a2
SumLare <mrhard95@gmail.com>
2017-04-23 12:12:26 +0300

    Rename process methods to eliminate

--- pnf/main.py
+++ pnf/main.py
@@ -4,18 +4,18 @@ import re
 def main():
     formula = input("Transform your formula to PNF: ")
     while formula:
-        if "->" in formula: formula = process_implication(formula)
-        if "-" in formula: formula = process_negation(formula)
+        if "->" in formula: formula = eliminate_implication(formula)
+        if "-" in formula: formula = eliminate_negation(formula)
         print(formula)
         formula = input("Transform your formula to PNF: ")
 
-def process_implication(formula):
+def eliminate_implication(formula):
     statements = formula.split("->")
     statements[0] = "-" + statements[:2][0].strip()
     first_two = " |".join(statements[:2])
     return first_two + "->{0}".format(''.join(statements[2:]))
 
-def process_negation(formula):
+def eliminate_negation(formula):
     if "--" in formula:
         return re.sub("--", "", formula)
 
