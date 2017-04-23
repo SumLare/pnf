89c8efee24e93b6de7f98db0c59686fb1e4d2fe5
SumLare <mrhard95@gmail.com>
2017-04-24 02:07:41 +0300

    Fix inward negation

--- pnf/main.py
+++ pnf/main.py
@@ -10,17 +10,24 @@ def main():
 def eliminate_implication(formula):
     statements = formula.split("->")
     statements[0] = "~" + statements[:2][0].strip()
-    first_two = " |".join(statements[:2])
-    return first_two + "->{0}".format(''.join(statements[2:]))
+    return " |".join(statements[:2])
 
 def inward_negation(formula):
-    index = formula.find("~∃") if formula.find("~∃") is not -1 else formula.find("~∀")
+    index = formula.find("~(∃") if formula.find("~(∃") is not -1 else formula.find("~(∀")
     while index is not -1:
         replace_index = index + 2
-        newformula = formula[:index] + formula[(index+1):]
-        formula = newformula[:replace_index] + "~" + newformula[replace_index:]
-        index = formula.find("~∃") if formula.find("~∃") is not -1 else formula.find("~∀")
+        without_negation = formula[:index] + formula[index+2:]
+        literal = without_negation[:index] + without_negation[index+1:]
+        formula = literal[:index] + "(" + quantifier(without_negation[index]) + \
+                  literal[index:replace_index] + "~" + literal[replace_index:]
+        index = formula.find("~(∃") if formula.find("~(∃") is not -1 else formula.find("~(∀")
     return re.sub("~~", "", formula)
+
+def quantifier(quantifier):
+    if quantifier == "∃":
+        return re.sub("∃", "∀", quantifier)
+    else:
+        return re.sub("∀", "∃", quantifier)
 
 def parse(formula):
     if "->" in formula: formula = eliminate_implication(formula)
