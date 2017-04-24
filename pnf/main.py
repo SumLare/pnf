a865a2ced3960d2cd9586a434b5e7524ac1890b0
SumLare <mrhard95@gmail.com>
2017-04-24 16:42:17 +0300

    Eliminate equivalence

--- pnf/main.py
+++ pnf/main.py
@@ -7,12 +7,19 @@ def main():
         parse(formula)
         formula = input("Transform your formula to PNF: ")
 
+def eliminate_equivalence(formula):
+    statements = formula.split("<->")
+    left_literal = "(~" + statements[0].strip() + " | " + statements[1].strip() + ")"
+    right_literal = "(" + statements[0].strip() + " | ~" + statements[1].strip() + ")"
+    return " & ".join([left_literal, right_literal])
+
 def eliminate_implication(formula):
     statements = formula.split("->")
     statements[0] = "~" + statements[:2][0].strip()
     return " |".join(statements[:2])
 
 def inward_negation(formula):
+    if formula.find("~∃") is not -1: raise SyntaxError("Use parentheses with quantifiers")
     index = formula.find("~(∃") if formula.find("~(∃") is not -1 else formula.find("~(∀")
     while index is not -1:
         replace_index = index + 2
@@ -30,6 +37,7 @@ def quantifier(quantifier):
         return re.sub("∀", "∃", quantifier)
 
 def parse(formula):
+    if "<->" in formula: formula = eliminate_equivalence(formula)
     if "->" in formula: formula = eliminate_implication(formula)
     if "~" in formula: formula = inward_negation(formula)
     return print(formula)
