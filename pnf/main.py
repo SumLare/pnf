69e23315b59ee917e19dd13c598f7d287fe9e17e
SumLare <mrhard95@gmail.com>
2017-05-02 03:40:33 +0300

    Standardize formula, fix imlication elimination

--- pnf/main.py
+++ pnf/main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
+import string
+import random
 
 def main():
     formula = input("Transform your formula to PNF: ")
@@ -14,8 +16,14 @@ def eliminate_equivalence(formula):
     return " & ".join([left_literal, right_literal])
 
 def eliminate_implication(formula):
-    statements = formula.split("->")
-    statements[0] = "~" + statements[:2][0].strip()
+    statements_in_params = re.findall("\((.*?)\)", formula)
+    if "->" in statements_in_params:statements = formula.split("->")
+    for statement in statements_in_params:
+        if "->" in statement:
+            statements = statement.split("->")
+            statements[0] = "~" + statements[:2][0].strip()
+        else
+            statements = formula.split("->")
     return " |".join(statements[:2])
 
 def inward_negation(formula):
@@ -25,21 +33,35 @@ def inward_negation(formula):
         replace_index = index + 2
         without_negation = formula[:index] + formula[index+2:]
         literal = without_negation[:index] + without_negation[index+1:]
-        formula = literal[:index] + "(" + quantifier(without_negation[index]) + \
+        formula = literal[:index] + "(" + change_quantifier(without_negation[index]) + \
                   literal[index:replace_index] + "~" + literal[replace_index:]
         index = formula.find("~(∃") if formula.find("~(∃") is not -1 else formula.find("~(∀")
     return re.sub("~~", "", formula)
 
-def quantifier(quantifier):
+def change_quantifier(quantifier):
     if quantifier == "∃":
         return re.sub("∃", "∀", quantifier)
     else:
         return re.sub("∀", "∃", quantifier)
 
+def standardize(formula):
+    quantifier_pattern = re.compile("∀|∃")
+    quantifier_indexes = [q.start(0) for q in quantifier_pattern.finditer(formula)]
+    for q_index in quantifier_indexes:
+        new_var = random.choice(string.ascii_letters[:26])
+        if formula[q_index+2] == "(":
+            close_parant_index = formula[q_index:].index(")")
+            literal_in_parant = formula[q_index:][3:close_parant_index]
+            changed_literal_in_parant = re.sub(r"[a-z]", new_var, literal_in_parant)
+            return formula[:q_index+1] + new_var + "(" + changed_literal_in_parant + formula[q_index:][close_parant_index:]
+        else:
+            return formula[:q_index+1] + new_var + formula[q_index+2] + new_var + formula[q_index+4:]
+
 def parse(formula):
     if "<->" in formula: formula = eliminate_equivalence(formula)
     if "->" in formula: formula = eliminate_implication(formula)
     if "~" in formula: formula = inward_negation(formula)
+    formula = standardize(formula)
     return print(formula)
 
 if __name__ == '__main__':
